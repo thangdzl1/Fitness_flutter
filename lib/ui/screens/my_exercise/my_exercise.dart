d1e455eb0c1b0b1dc2f639a1e909c18f5f3aeeff
BQcThang <quocthang6a1@gmail.com>
2025-05-28 04:51:51 +0700

    update myexcercise_api name, api hooks and create those funtion in my_excercise screen

--- lib/ui/screens/my_exercise/my_exercise.dart
+++ lib/ui/screens/my_exercise/my_exercise.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart'; // Thêm package TableCalendar
-import '../../../services/api_myexercise.dart';
+import '../../../services/myexercise_api.dart';
 import '../../widgets/bottom_nav_bar.dart'; // BottomNavBar có sẵn
 
 class MyMealScreen extends StatefulWidget {
